257f98ab0acd0f24c21fe0bc60d21fd696c3d7c4
Robbe <robbeipers@outlook.com>
2022-07-02 12:49:02 +0200

    init commit

---
+++ src/main/java/com/robbe/studentplatform/Studentplatform.java
@@ -0,0 +1,13 @@
+package com.robbe.studentplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Studentplatform {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Studentplatform.class, args);
+    }
+
+}
---
+++ src/test/java/com/robbe/studentplatform/StudentplatformTests.java
@@ -0,0 +1,13 @@
+package com.robbe.studentplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentplatformTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
